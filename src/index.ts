383af36ac3b8c8351528a29f10be52ce010f71e9
gutovesco <guto.vesco@hotmail.com>
2020-01-27 00:09:11 -0300

    gotta start from scratch

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,14 @@
 import {createStore} from 'redux';
 
 //ACTION -> what you wanna do, LIKE: INCREMENT
-const increment = () => {
-    return{
-        type: 'INCREMENT'
-    }
-}
-const decrement = () => {
-    return{
-        type: 'DECREMENT'
-    }
-}
+
 
 //REDUCER -> CHECK THE ACTION AND CHANGE THE BASED ON THE RESULT OF THE ACTION
-const counter = (state = 0, action) =>{
-    switch(action.type){
-        case "INCREMENT":
-            return state + 1;
-    }
-}
+
 
 //STORE -> ESTADO GLOBALIZADO
-let store = createStore(counter);
-store.subscribe(() => console.log(store.getState())); //mostra no console da pagina
+ //mostra no console da pagina
 
 //DISPATCH -> EXECUTES
-store.dispatch(increment());
+
 
